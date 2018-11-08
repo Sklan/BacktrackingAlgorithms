a4a07c3fb164e72ec6e21b3d588ffe163b523488
Sklan <Sklan@users.noreply.github.com>
2018-11-09 01:22:02 +0530

    Add files via upload

---
+++ Hangman.h
@@ -0,0 +1,29 @@
+//
+// Created by Shaurya Saklani on 14/07/17.
+//
+
+#include <string>
+#include <random>
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+#ifndef PROGRAMMING_ABSTRACTION_IN_C_RANDWORD_H
+#define PROGRAMMING_ABSTRACTION_IN_C_RANDWORD_H
+
+vector <string> initDictionary();
+/*
+ * This function loads a file and reads words from it.
+ */
+
+vector <char> chooseRandomWord();
+/*
+ * This function calls init dictionary selects a random word from the file and returns it.
+ */
+
+void Hangman();
+/*
+ * Executes Hangman game
+ */
+#endif //PROGRAMMING_ABSTRACTION_IN_C_RANDWORD_H
---
+++ HashMap.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Shaurya Saklani on 16/09/17.
+//
+
+#include "HashMap.h"
